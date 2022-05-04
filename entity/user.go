fc5284f50c0044fb1290164e50008d285ce0d52e
Adi Putra <adiputrapermana@gmail.com>
2022-05-04 21:06:58 +0700

    introduce user management feature

---
+++ entity/user.go
@@ -0,0 +1,9 @@
+package entity
+
+type User struct {
+	ID       string `json:"ID"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+}
--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 var (
 	httpRouter     router.Router             = router.NewMuxRouter()
 	pingController controller.PingController = controller.NewPingController()
+	userController controller.UserController = controller.NewUserController()
 )
 
 func main() {
 	const port string = "8080"
 
 	httpRouter.Get("/api/ping", pingController.Ping)
+
+	// User
+	httpRouter.Get("/api/users", userController.GetUsers)
+	httpRouter.Get("/api/users/{user}", userController.GetUser)
+	httpRouter.Post("/api/users", userController.CreateUser)
+	httpRouter.Put("/api/users/{user}", userController.UpdateUser)
+	httpRouter.Delete("/api/users/{user}", userController.DeleteUser)
 
 	httpRouter.Serve(port)
 }
