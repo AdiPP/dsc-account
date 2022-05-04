14aa01ff19264779a12ee0010df6207b157d5663
Adi Putra <adiputrapermana@gmail.com>
2022-05-04 21:11:47 +0700

    chore: add post, put, patch, and delete http methods on router

--- http/mux_router.go
+++ http/mux_router.go
@@ -18,7 +18,23 @@ func NewMuxRouter() Router {
 }
 
 func (m *MuxRouter) Get(uri string, f func(w http.ResponseWriter, r *http.Request)) {
-	dispatcher.HandleFunc(uri, f).GetMethods()
+	dispatcher.HandleFunc(uri, f).Methods(http.MethodGet)
+}
+
+func (m *MuxRouter) Post(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	dispatcher.HandleFunc(uri, f).Methods(http.MethodPost)
+}
+
+func (m *MuxRouter) Put(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	dispatcher.HandleFunc(uri, f).Methods(http.MethodPut)
+}
+
+func (m *MuxRouter) Patch(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	dispatcher.HandleFunc(uri, f).Methods(http.MethodPatch)
+}
+
+func (m *MuxRouter) Delete(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	dispatcher.HandleFunc(uri, f).Methods(http.MethodDelete)
 }
 
 func (m *MuxRouter) Serve(port string) {
